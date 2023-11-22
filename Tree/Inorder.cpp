05946a73587cfd88cc99dd468a2c9ea747e434d5
Jahidul Hasan Suhel <108091317+JahidulHasanSuhel@users.noreply.github.com>
2023-11-23 02:09:08 +0600

    Inorder.cpp

---
+++ Tree/Inorder.cpp
@@ -0,0 +1,12 @@
+// just remeber the formula -> LNR = Left Node Right
+void inorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    inorder(root->left);
+    cout << root-> data << " ";
+    inorder(root->right);
+
+}
