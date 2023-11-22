4428005df53500ae2ac1f399d5683d2144e3795f
Jahidul Hasan Suhel <108091317+JahidulHasanSuhel@users.noreply.github.com>
2023-11-23 02:06:58 +0600

    Level Order Traversal.cpp

---
+++ Tree/Level Order Traversal.cpp
@@ -0,0 +1,30 @@
+void levelOrderTraversal(node* root) {
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) { 
+            //previous level complete the traverse
+            cout << endl;
+            if(!q.empty()) { 
+                //queue still has some child ndoes
+                q.push(NULL);
+            }  
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+
+}
