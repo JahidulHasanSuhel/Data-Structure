1d6829d58061ec470fdf61ad4b65cdafb1a4cec9
Jahidul Hasan Suhel <108091317+JahidulHasanSuhel@users.noreply.github.com>
2023-11-23 02:15:04 +0600

    Create Postorder.cpp

---
+++ Tree/Postorder.cpp
@@ -0,0 +1,12 @@
+// just remeber the formula -> LRN = Left Right Node 
+void postorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root-> data << " ";
+
+}
